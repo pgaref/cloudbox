90c73f295f046e47115ab849fdfe36bd62f3d007
Panagiotis Garefalakis <pgaref@ics.forth.gr>
2014-02-19 01:11:55 +0200

    fixes

--- c/udp_client.c
+++ c/udp_client.c
@@ -77,8 +77,8 @@ int udp_packet_encode(msg_type_t type, char * client_name, int tcp_port, time_t 
 	}
 	packet_count+=2;
 	
-	memcpy(&packet_to_send[packet_count], &clk, 8);
-	packet_count+=8;
+	/*memcpy(&packet_to_send[packet_count], &clk, 8);
+	packet_count+=8;*/
 	if(is_big_endian()){
 		SWAP(mod_time);
 		memcpy(&packet_to_send[packet_count], &mod_time,8);
@@ -124,8 +124,8 @@ int udp_file_packet_encode(msg_type_t type, char * client_name, int tcp_port, ti
 	}
 	packet_count+=2;
 	
-	memcpy(&packet_to_send[packet_count], &clk, 8);
-	packet_count+=8;
+	/*memcpy(&packet_to_send[packet_count], &clk, 8);
+	packet_count+=8;*/
 	if(is_big_endian()){
 		SWAP(curr_time);
 		memcpy(&packet_to_send[packet_count], &curr_time,8);
@@ -161,7 +161,7 @@ int udp_file_packet_encode(msg_type_t type, char * client_name, int tcp_port, ti
 	printf("---Encoding Complex message---\n");
 	printf("TYPE : %d \n", type);
 	printf("Pak : %d %d \n", packet_to_send[0], packet_to_send[1]);
-	printf("Tcp Port: %u \n", b);
+	printf("File name %s\n", filename);
 	printf("Size: %d\n", packet_count);
 	printf("Current Time %s \n",ctime(&clk));
 	printf("File modification Time %s \n",ctime(&curr_time));
