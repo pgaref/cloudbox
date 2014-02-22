3e2dc2e816ef2712ab148a95cfaa412f687d56a0
Panagiotis Garefalakis <pgaref@ics.forth.gr>
2014-02-22 04:58:55 +0200

    iiii

--- c/cloudbox.c
+++ c/cloudbox.c
@@ -439,8 +439,8 @@ void dir_list_free(struct dir_files_status_list * dirList){
 	//printf("-> Trying to Free memory . .\n");
 	SGLIB_LIST_MAP_ON_ELEMENTS(struct dir_files_status_list, dirList, l, next, {
 			free(l->filename);
-			free(l->owner);
-			free(l->group);
+			//free(l->owner);
+			//free(l->group);
 			free(l);
 	});
 }
